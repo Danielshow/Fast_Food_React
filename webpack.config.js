b0c80c02561e3a275b435e09b2fa4b770d9cbb8b
Daniel Shotonwa <danielshotonwa53@gmail.com>
2018-11-25 08:07:00 +0100

    [Chore #162050116] Setup Webpack for bundling Code
    - Set up eslint to follow AirBnb Style guide
    - Add Webpack and Webpack-server to bundle code
    - Add the various plugins needed css-loader, style-loader and html-webpack-plugins

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.min.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+};
